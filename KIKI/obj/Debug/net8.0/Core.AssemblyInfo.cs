c641ae1f3bdefbfbaf7a179ef6bc30519dea9292
ssuuji <sub9399@naver.com>
2026-05-18 17:06:54 +0900

    Commit to master

--- KIKI/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ KIKI/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f20d7450a5eb485d4b49013eeccbf379c9932593")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c52de26cb368bf2a9c9fbdb8ee19aaf1a465355")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f20d7450a5eb485d4b49013eeccbf379c9932593")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c52de26cb368bf2a9c9fbdb8ee19aaf1a465355")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
